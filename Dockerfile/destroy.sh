8cf03793e5ffde156e634833b53ccb929ca219c0
Moutaz Muhammad <85679864+moutazmuhammad@users.noreply.github.com>
2025-04-25 09:41:18 +0300

    Update destroy.sh

--- Dockerfile/destroy.sh
+++ Dockerfile/destroy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-###########################################################################
+##################################################################################################################
 # Author: Moutaz Muhammad <moutazmuhamad@gmail.com>
 # Script to stop containers and remove volumes for docker-compose files
 # found inside directories named odoo11 or odoo14, no need for full path
-###########################################################################
+
+# curl -s https://raw.githubusercontent.com/moutazmuhammad/odoo-docker-compose/main/Dockerfile/destroy.sh | bash
+
+##################################################################################################################
 set -e
 
 # Default search path (root of the file system or user's home, or adjust as needed)
@@ -15,7 +18,7 @@ TARGET_DIRS=("EXPERT/odoo11" "EXPERT/odoo14")
 
 # Function to find docker-compose.yml files inside directories named odoo11 or odoo14
 stop_containers_and_remove_volumes() {
-    echo "Searching for docker-compose.yml files inside directories named 'odoo11' or 'odoo14' in $SEARCH_PATH..."
+    echo "[INFO] Searching for docker-compose.yml files inside directories named 'odoo11' or 'odoo14' in $SEARCH_PATH..."
 
     # Use find to search for directories named odoo11 or odoo14 containing docker-compose.yml files
     for dir in "${TARGET_DIRS[@]}"; do
@@ -23,14 +26,14 @@ stop_containers_and_remove_volumes() {
         find "$SEARCH_PATH" -type f -path "*/$dir/docker-compose.yaml" 2>/dev/null | while read docker_compose_file; do
             # Extract the directory containing docker-compose.yml
             docker_compose_dir=$(dirname "$docker_compose_file")
-            echo "Stopping containers and removing volumes in directory: $docker_compose_dir"
+            echo "[INFO] Stopping containers and removing volumes in directory: $docker_compose_dir"
             
             # Change to the directory containing the docker-compose.yml file
             cd "$docker_compose_dir" || continue
             
             # Stop containers and remove volumes
             docker-compose down -v
-            echo "Containers stopped and volumes removed in: $docker_compose_dir"
+            echo "[INFO] Containers stopped and volumes removed in: $docker_compose_dir"
         done
     done
 }
@@ -39,7 +42,7 @@ sudo rm -rf /usr/local/bin/restart-odoo11
 sudo rm -rf /usr/local/bin/restart-odoo14
 
 # Main execution
-echo "Uninstalling Docker Compose environments..."
+echo "[INFO] Uninstalling Docker Compose environments..."
 
 # Stop containers and remove volumes for matching directories
 stop_containers_and_remove_volumes
